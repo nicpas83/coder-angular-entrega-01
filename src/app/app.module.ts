6c5ceace682231cebfbd2d1dcc416b3b5b45d792
nicpas83 <nicpas@gmail.com>
2022-11-22 16:07:26 -0300

    entrega01 - layout basico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { ToolbarComponent } from './layout/toolbar/toolbar.component';
+import { MainComponent } from './layout/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ToolbarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
